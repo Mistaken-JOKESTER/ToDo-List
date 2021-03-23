dd7ea4f76acf1b9a1084fd09aa48fd0b7cc6e903
ankit sharma <ankitsharmarock99@gmail.com>
2021-03-23 23:03:24 +0530

    register form

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from './components/Header';
 import LoginForm from './components/LoginForm';
@@ -7,16 +7,29 @@ import RegisterForm from './components/RegisterForm';
 export const TokenContext = React.createContext()
 
 function App() {
+
   const [authToken, setAuthToken] = useState('')
+  const [showForm, setShowForm] = useState(-1)
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('TodoListAuthToken')
+    if(token === null || token === ''){
+      setAuthToken(null)
+    }
+
+    console.log(authToken?true:false, showForm === -1? true: false)
+  }, [])
+
+  const formHandler = (value) => {
+    setShowForm(value)
+  }
+
   return (
     <div className="App">
       <Header />
       <main>
-        <TokenContext.Provider value={{authToken, setAuthToken:setAuthToken()}}>
-          <LoginForm />
-        </TokenContext.Provider>
-        
-        <RegisterForm />
+        <LoginForm isLoggedIn={authToken?true:false} show={showForm === 1?true:false} setShowForm={setShowForm}/>
+        <RegisterForm isLoggedIn={authToken?true:false} show={showForm === -1? true: false} setShowForm={setShowForm}/>
       </main>
       
     </div>
